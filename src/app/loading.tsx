2d5797c220a728067ec4278e65870c02576fdd84
Nasuschnyi <91079054+Nasuschnyi@users.noreply.github.com>
2024-10-18 09:52:01 +0300

    Add initial dependencies

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+'use client';
+export default function Loading() {
+	return (
+		<div
+			style={{ textAlign: 'center', marginTop: '50vh', fontSize: '4vh' }}
+		>
+			<p>Loading...</p>
+		</div>
+	);
+}
